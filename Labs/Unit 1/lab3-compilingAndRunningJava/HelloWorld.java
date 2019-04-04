156d62a7bc6010aa979300d2c9329d45dfe630d3
dalf2598 <alejofon98@hotmail.com>
2019-04-04 18:40:31 -0500

    lab3 added

---
+++ Labs/Unit 1/lab3-compilingAndRunningJava/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+
+  public static void main(String[] args) {
+    System.out.println("Hello world of programmers from Garcia");
+	System.out.println("Bye world of developers from David Alejandro Lopez Fonseca");
+    System.out.println("Bye world of programmers from Renan Andres Garcia Yanez");
+  }
+ 
+
+}
+
