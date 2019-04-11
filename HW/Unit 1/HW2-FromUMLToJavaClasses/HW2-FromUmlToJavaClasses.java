175be619f37cfa7e029db3f63e5adaef84c9eaa5
dalf2598 <alejofon98@hotmail.com>
2019-04-10 21:42:05 -0500

    tomorrow's homework

---
+++ HW/Unit 1/HW2-FromUMLToJavaClasses/HW2-FromUmlToJavaClasses.java
@@ -0,0 +1,78 @@
+public class Egg{
+	private int id;
+}
+
+public class Chicken {
+	private int id;
+	private String name; 
+	private String color;
+	private int age;
+	private bool molting;
+	
+	public void doStuff(int forTime){
+		//TODO
+	}
+	
+	private void cluck () {
+		//TODO
+	}
+	private void wander () {
+		//TODO
+	}
+	private void eat () {
+		//TODO
+	}
+	private void drink () {
+		//TODO
+	}
+	private void poop () {
+		//TODO
+	}
+	private Egg leyAnEgg(){
+		
+		Egg egg = new Egg();
+		return egg;
+	}
+}
+
+public class Coop {
+	
+	private int id;
+	private Chicken[] chicken;
+	
+	private void add (Chiken chicken) {
+		//TODO
+	}
+	
+	private void remove (int chickenld) {
+		//TODO
+	}
+	
+	private void resetIteration () {
+		//TODO
+	}
+	
+	private Chicken next(){
+		//TODO
+	}
+}
+
+public class ChikenFarmer {
+		
+	private String name;
+	private Coop[] coop;
+	
+	private void add (Coop coop) {
+		//TODO
+	}
+	private void add (int coopld) {
+		//TODO
+	}
+	private void resetIteration () {
+		//TODO
+	}
+	private Coop next(){
+		//TODO
+	}
+}	
+
