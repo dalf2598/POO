98a43722f4777607b2e177fe95caea197e72557d
dalf2598 <alejofon98@hotmail.com>
2019-05-08 21:32:58 -0500

    unit 2 labs added

---
+++ Labs/Unit 2/lab7-inheritance/Fundamentals/src/ec/edu/espe/lab7/fundamentals/fundamentals.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.lab7.fundamentals;
+import ec.edu.espe.lab7.fundamentals.inheritance.Animal;
+import ec.edu.espe.lab7.fundamentals.inheritance.Mammal;
+/**
+ *
+ * @author David Lopez
+ */
+public class fundamentals {
+    public  static  void  main ( String [] args ) {
+        
+        Animal animal = new Mammal(2,5,"Lucy");
+        System.out.println(animal);
+        animal.print();
+        
+        Mammal mammal = new Mammal(2,4,"Firulais");
+        mammal.print();
+        
+    }
+}
