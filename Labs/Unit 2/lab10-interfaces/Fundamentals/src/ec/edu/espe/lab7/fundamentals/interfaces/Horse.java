0bf4f67e6cd80ff148f83aa0712bf21b86f727e8
dalf2598 <alejofon98@hotmail.com>
2019-05-15 23:57:47 -0500

    lab 10 re uploaded

---
+++ Labs/Unit 2/lab10-interfaces/Fundamentals/src/ec/edu/espe/lab7/fundamentals/interfaces/Horse.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.lab7.fundamentals.interfaces;
+
+/**
+ *
+ * @author Labs-DCCO
+ */
+public class Horse extends WildAnimal implements Pet,Animal {
+
+    @Override
+    public boolean walk(int timeInSeconds, int distance) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
