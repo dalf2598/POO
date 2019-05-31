a2a894d5b2c68f78bff7e54e0e758e5deafa2c02
dalf2598 <alejofon98@hotmail.com>
2019-05-30 23:57:28 -0500

    lab 20 added

---
+++ Labs/Unit 2/lab20-widget/ProductAndDivision/src/ecu/edu/espe/productanddivision/view/ProductAndDivision.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ecu.edu.espe.productanddivision.view;
+import ecu.edu.espe.productanddivision.controller.Operations;
+
+/**
+ *
+ * @author David Lopez
+ */
+public class ProductAndDivision {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //division variables
+        String dividen="5";
+        String divisor="0";
+        float quotient;
+        
+        //product variables
+        String stringOriginalValue="5";
+        String stringTimesToAdd="-100";
+        int result=0;
+        
+        //Division code
+        float tempDividen= Operations.validateDataDivision(dividen);
+        float tempDivisor= Operations.validateDataDivision(divisor);
+        
+        quotient=Operations.division(tempDividen, tempDivisor);
+        System.out.println(quotient);
+        
+        //Product code
+        result= Operations.multiplication(stringOriginalValue,stringTimesToAdd);
+        System.out.println(result);
+        
+    }
+    
+    
+}
